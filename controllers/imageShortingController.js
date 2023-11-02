1d8091ede31447e3b38535e39662d8be5718c6b3
Safaa-Ebrahim <ebrasafa@gmail.com>
2023-11-02 23:14:40 +0200

    use bitly to shorten the link

--- controllers/imageShortingController.js
+++ controllers/imageShortingController.js
@@ -2,21 +2,44 @@ const axios = require("axios");
 
 module.exports.postShortUrl = async (request, response, next) => {
   const db = request.db;
-  const url = request.query;
-  const LongUrl = `https://api.shrtco.de/v2/shorten?url=${url}`;
-  axios
-    .post(LongUrl)
-    .then((res) => {
-      const { short_link: shortLink } = res.data.result;
-      console.log(res.data.result);
-      response.status(201).json({ shortLink });
-      //   Save the shortened link to Firestoreconst
-      db.collection("Urls")
-        .add({ original_link: url, shortLink })
-        .then((docRef) => {
-          response.status(201).json({ id: docRef.id, shortLink });
-        })
-        .catch((error) => next(error));
-    })
-    .catch((error) => next(error));
+  const accessToken = "68ae3fc91849e9feb258179d7877f6d37fcb86d4";
+  const apiUrl = `https://api-ssl.bitly.com/v4/shorten`;
+  const longUrl = request.query.url;
+
+  try {
+    const { data } = await axios.post(
+      apiUrl,
+      {
+        long_url: longUrl,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    await db
+      .collection("Urls")
+      .add({ original_link: longUrl, short_link: data.link });
+    response.status(201).json({ shortLink: data.link });
+  } catch (error) {
+    next(error);
+  }
 };
+
+// try {
+//   const LongUrl = `https://api.shrtco.de/v2/shorten?url=${req.query.url}`;
+
+//   // Make an Axios request to the shortening service
+//   const axiosResponse = await axios.post(LongUrl);
+
+//   // Save the shortened URL in the database
+//   const shortLink = axiosResponse.data.result.full_short_link;
+//   await db.collection("Urls").add({ shortLink });
+
+//   response.status(201).json({ shortLink: data.link });
+// } catch (error) {
+//   next(error);
+// }
